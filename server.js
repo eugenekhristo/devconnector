46c33ce4bf4adf1194c6592b58bcbafdfd5a1e16
Eugene Khristo <marteau9292@gmail.com>
2019-07-29 09:54:38 +0300

    Add test routes

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const connectDb = require('./config/connectDb');
 const app = express();
 connectDb();
 
-app.get('/', (req, res) => {
-  res.send('HI!!!!');
-});
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
